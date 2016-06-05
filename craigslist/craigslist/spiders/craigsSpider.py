b54ee8d27c18dd466ff3d0602d294b75003a778d
Shreya Dahal <shreyadahal@gmail.com>
2016-06-05 21:18:52 +0545

    added area attribute and formating changes.

--- craigslist/craigslist/spiders/craigsSpider.py
+++ craigslist/craigslist/spiders/craigsSpider.py
@@ -13,16 +13,20 @@ class CraigsSpider(scrapy.Spider):
     def parse(self, response):
         item = CraigslistItem()
 
-        title = response.xpath('//span[@id="titletextonly"]/text()').extract_first()
-        desc = '\n'.join( line.strip() for line in response.xpath('//section[@id="postingbody"]/text()').extract() )
+        title = response.xpath('//span[@id="titletextonly"]/text()')\
+                .extract_first()
+        desc = '\n'.join(line.strip() for line in \
+                response.xpath('//section[@id="postingbody"]/text()').extract())
         price = response.xpath('//span[@class="price"]/text()').extract_first()
-        bedroom, bathroom = [ int(x) for x in response.xpath('//p[@class="attrgroup"]/span/b/text()').extract() ]
+        attributes = [ int(x) for x in response\
+                .xpath('//p[@class="attrgroup"]/span/b/text()').extract() ]
 
         item['title'] = title
         item['desc'] = desc
         item['price'] = price
-        item['bedroom'] = bedroom
-        item['bathroom'] = bathroom
+        item['bedroom'] = attributes[0]
+        item['bathroom'] = attributes[1]
+        item['area'] = attributes[2] if len(attributes) == 3 else None
 
         reply_url = response.url.replace('pnl','reply/tpa')[:-5]
         request = scrapy.Request(reply_url, callback=self.parse_contact)
@@ -34,10 +38,12 @@ class CraigsSpider(scrapy.Spider):
 
         phone_xpath, email_xpath, _, _ = response.xpath('//ul')
 
-        phone = ' '.join(phone_xpath.xpath('li/text()').extract_first().split()[1:])
+        phone = ' '.join( 
+            phone_xpath.xpath('li/text()').extract_first().split()[1:]
+            )
         email = email_xpath.xpath('li/a/text()').extract_first()
 
         item['phone'] = phone
         item['email'] = email
 
-        return item
+        yield item
